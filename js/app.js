b7e69f7d57ab63f61014fd1b70e4422d27d4dc74
adelomer215 <adelimer5533@gmail.com>
2021-09-29 12:08:01 +0200

    rebuild the projuct agin

--- js/app.js
+++ js/app.js
@@ -1,47 +1,75 @@
-const creatorButton = document.getElementById('create');
-const theBode = document.querySelector('main');
-const navbarList = document.getElementById('nav-bar-menu');
-// start build the function that creat the sections
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
 
-let sectionNumber = 0;
-const createSectionAndNavList = function () {
-    sectionNumber++;
-    const sectionContant = `<section id="section${sectionNumber}"  data-nav="Section ${sectionNumber}">
-        <div class="landing__container">
-            <h2>Section ${sectionNumber}</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra
-            dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus
-            imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget
-            bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet
-            elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo
-            nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie
-            semper in tellus. Sed congue et odio sed euismod.</p>
-            <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel
-            luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur
-            porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-        </div>
-    </section>`;
-    const liContent = `<li><a href="#section${sectionNumber}" class="menu__link" data-nav="section${sectionNumber}">section${sectionNumber}</a></li>`;
-    navbarList.insertAdjacentHTML("beforeend", liContent);
+/**
+ * Define Global Variables
+ *
+*/
+let sections = document.querySelectorAll('section');
+const navMenu = document.querySelector('[data-menu]');
+const mainElement = document.querySelector('main');
+const theUl = document.querySelector('ul');
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
 
-    theBode.insertAdjacentHTML("beforeend", sectionContant);
-};
+const theButtom = document.createElement('button');
+theButtom.setAttribute('class', 'creat');
+theButtom.innerHTML = 'Creat Section';
+navMenu.appendChild(theButtom);
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
 
-creatorButton.onclick = createSectionAndNavList;
-// start build the function that creat the navlist
+// build the nav
+for (let i = 0; i < sections.length; i++) {
+    const navList = document.createElement('li');
+    navList.innerHTML = `<a href="#section${i + 1}" class="menu__link" data-nav="section${i + 1}">section ${i + 1}</a>`;
+    theUl.appendChild(navList);
+}
+let sectionsLength = sections.length;
+theButtom.addEventListener('click', (params) => {
+    const theSection = document.querySelector('section').cloneNode(true);
+    const landingCon = theSection.lastElementChild;
+    const theH2 = landingCon.firstElementChild;
+    theSection.id = `section${sectionsLength + 1}`;
+    theSection.setAttribute('data-nav', `section ${sectionsLength + 1}`);
+    theSection.classList = '';
+    theH2.innerHTML = `Section ${sectionsLength + 1}`;
+    const newLi = `<li><a href="#section${sectionsLength + 1}" class="menu__link" data-nav="section${sectionsLength + 1}">section${sectionsLength + 1}</a></li>`;
+    sectionsLength++;
+    mainElement.appendChild(theSection);
+    // theUl.appendChild(newLi);
+    theUl.insertAdjacentHTML('beforeend', newLi);
+    sections++;
+});
 
-// let listLength = 0;
-// const createNavList = () => {
-//     listLength++;
-//     const liContent = `<li><a href="#section${listLength}" class="menu__link">section${listLength}</a></li>`;
-//     navbarList.insertAdjacentHTML("beforeend", liContent);
+// Scroll to anchor ID using scrollTO event
 
-// }
-// creatorButton.onclick = createNavList;
-
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
 window.onscroll = function () {
-    document.querySelectorAll("section").forEach(function (active) {
-        let theActive = navbarList.querySelector(`[data-nav=${active.id}]`);
+    document.querySelectorAll('section').forEach(function (active) {
+        let theActive = theUl.querySelector(`[data-nav = ${active.id}]`);
         if (active.getBoundingClientRect().top >= -200 && active.getBoundingClientRect().top <= 150) {
             active.classList.add("your-active-class");
             theActive.classList.add("active-link");
@@ -50,19 +78,12 @@ window.onscroll = function () {
             active.classList.remove("your-active-class");
             theActive.classList.remove("active-link");
         }
-        // console.log(activeLink);
     });
 };
+// Build menu
 
-// make the page bulid 4 section by defult when looding 
-for (let i = 1; i < 5; i++) {
-    createSectionAndNavList();
-}
+// Scroll to section on link click
+
+// Set sections as active
 
 
-/*
-    In the end, I would like to apologize for something.
-    I thought that starting the construction of the project is tomorrow and that there will be a specific time to work on the project,
-    but one of my friends told me that this is the last day for receiving and that I misunderstood,
-    so I turned to implement the project as soon as possible and I hope I didn't make many mistakes, and thank you for this wonderful amount of information and beautiful explanation.
-*/
